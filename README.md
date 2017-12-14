d155b3570aa1ac141fb829f181184ce5854a58e0
Danny Markov <d.markov94@gmail.com>
2017-12-14 15:26:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Bootstrap Better Nav
+
+###  Tiny library that replaces the default Bootstrap navbar collapse with an elegant off-screen menu.
+
+No configuration and no additional HTML markup required. Just include the library's JavaScript and CSS files, and your new navigation is ready.
+
+<div align="center">
+![Demo](https://github.com/bootstrapstudio/bootstrap-better-nav/demo.gif)
+</div>
+
+## Usage
+
+The library works only in Bootstrap 4 projects and requires a valid Navbar to be present on the page.
+
+To use it, simply include its CSS and JavaScript in your HTML. You can find the necessary files in the `/dist` directory of this project. CDN versions are also available via unpkg.
+
+```html
+<!-- In <head> after the Bootstrap CSS. -->
+<link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css">
+
+<!-- At the end of <body>, after jQuery and the Bootstrap js -->
+<script src="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.js"></script>
+```
+
+Once the JavaScript is included in the page the library is automatically enabled. A new off-screen menu is added to the HTML, which is synced with the contents of your navbar automatically. 
+
+```html
+<div id="side-menu">
+  <button class="close"><span aria-hidden="true">×</span></button>
+  <div class="contents">
+      <!-- The items from the Bootstrap Navbar will be copied here by the library. -->
+  </div>
+</div>
+```
+
+Clicking on the navbar toggle button will show the off-screen menu instead of expanding the Bootstrap navbar.
+
+The menu has only basic styling, you can easily customize it with CSS. 
+
+
---
+++ dist/bootstrap-better-nav.js
@@ -0,0 +1,85 @@
+$(function() {
+    
+    var body = $('body');
+    var navbarCollapse = $('.navbar-collapse');
+
+
+
+    // Add the needed HTML elements for the plugin to work. 
+    // All the elements are styled in navbar-sidemnu.css.
+    
+    body.append('<div class="side-menu-overlay"></div>');
+    var overlay = $('.side-menu-overlay');
+
+    body.append('<div id="side-menu"></div>');
+    var sideMenu = $('#side-menu');
+
+    sideMenu.append('<button class="close"><span aria-hidden="true">×</span></button>')
+    var sideMenuCloseBtn = sideMenu.find('.close');
+
+    sideMenu.append('<div class="contents"></div>')
+    var sideMenuContents = sideMenu.find('.contents');
+
+
+
+    // This event is trigerred when the user clicks the navbar toggle button.
+
+    navbarCollapse.on('show.bs.collapse', function (e) {
+        // Stop the default navbar behaviour (don't open the collapse navigation).
+        e.preventDefault();
+
+        // Instead we copy the navbar contents and add them to our side menu.
+        var menuContent = $(this).html();    
+        sideMenuContents.html(menuContent);
+        
+        // Animate the side menu into frame.
+        slideIn();
+    });
+
+
+    // Hide the menu when the "x" button is clicked.
+    
+    sideMenuCloseBtn.on('click', function(e) {
+        e.preventDefault();
+        slideOut();
+    });
+
+    // Hide the menu when the overlay element is clicked.
+    
+    overlay.on('click', function(e) {
+        slideOut();
+    });
+
+    // Listen for changes in the viewport size.
+    // If the original navbar collapse is visible then the nav is expanded.
+    // Hide/Show the menu accordingly.
+    
+    $(window).resize(function(){
+        if(!navbarCollapse.is(":visible") && body.hasClass('side-menu-visible')) {
+            sideMenu.show();
+            overlay.show();
+        }
+        else {
+            sideMenu.hide();
+            overlay.hide();
+        }
+    });
+    
+    function slideIn() {
+        body.addClass('overflow-hidden');
+        sideMenu.show();
+        setTimeout(function() {    
+            body.addClass('side-menu-visible');
+            overlay.fadeIn();
+        }, 50);
+    }
+    
+    function slideOut() {
+        body.removeClass('side-menu-visible');
+        overlay.fadeOut();
+        setTimeout(function() {
+            sideMenu.hide();
+            body.removeClass('overflow-hidden');
+        }, 400);
+    }
+});
